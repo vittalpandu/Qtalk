f76d7ae61b2b5b3215b8547ddd485b79ee7897b3
root <root@ip-172-31-38-28.ap-south-1.compute.internal>
2024-08-10 10:47:27 +0000

    Qtalk

---
+++ src/main/java/Jspiders/App.java
@@ -0,0 +1,13 @@
+package Jspiders;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
